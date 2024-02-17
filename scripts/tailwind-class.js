6f3145f3093e9c86e8c3b68e7d9db0e7727f5d55
Koushik <koushikdas798@gmail.com>
2024-02-17 23:50:36 +0600

    Navigation Section Completed

---
+++ scripts/tailwind-class.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'primary-color': '#1DD100',
+                'secondary-color': '#030712',
+                'section-color': '#F7F8F8',
+                'color-80': '#030712cc',
+                'color-70': '#030712b3',
+                'color-60': '#03071299',
+                'color-50': '#03071280',
+                'color-40': '#03071266',
+            }
+        }
+    }
+}
